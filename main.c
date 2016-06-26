02817b70e5bd17e1febdcb841c3811c5e0a342a7
rdantzer <rdantzer@student.42.fr>
2016-06-26 23:35:57 +0200

    Lel

--- main.c
+++ main.c
@@ -10,6 +10,9 @@
 #include "safe_alloc.h"
 #include "scene.h"
 
+t_vec3 rt_color(const t_ray *r, t_scene *scene)
+;
+
 t_vec3 random_in_unit_sphere()
 {
     t_vec3  p;
@@ -18,6 +21,7 @@ t_vec3 random_in_unit_sphere()
     tmp.x = (float)drand48();
     tmp.y = (float)drand48();
     tmp.z = (float)drand48();
+    vec3_sub(&p, vec3_mul_f(&p, &tmp, 2.0), &g_vec3_identity);
     while (vec3_squared_length(&p) >= 1.0)
     {
         tmp.x = (float)drand48();
@@ -28,39 +32,42 @@ t_vec3 random_in_unit_sphere()
     return (p);
 }
 
+static t_vec3       retrace(const t_ray *r, t_scene *scene)
+{
+    t_vec3          color;
+
+    color = rt_color(r, scene);
+    vec3_mul_f(&color, &color, 0.5f);
+    return (color);
+}
+
 t_vec3 rt_color(const t_ray *r, t_scene *scene)
 {
     t_hit_record    record;
     t_precision     precision;
-    t_vec3 tmp, target, color, random;
+    t_vec3          tmp, target, random;
+    t_ray sub_ray;
 
     precision.min = 0.001;
     precision.max = MAXFLOAT;
     if (scene_raytrace(scene, r, precision, &record))
     {
         random = random_in_unit_sphere();
-        vec3_add(&target, vec3_add(&tmp, &record.normal, &record.pos), &random);
-        t_ray sub_ray;
-
+        vec3_add(&target, vec3_add(&target, &record.normal, &record.pos), &random);
         ray_assign(&sub_ray, &record.pos, vec3_sub(&tmp, &target, &record.pos));
-        return (rt_color(&sub_ray, scene));
+
+        return (retrace(&sub_ray, scene));
     }
     else {
-        t_vec3 uv;
-        t_vec3  v;
-        const t_vec3 gradient = {0.5f, 0.7f, 1.0f};
+        t_vec3  u;
         float   t;
+        t_vec3  lerp;
+        const t_vec3 gradient = {.5f, .7f, 1.f};
 
-        vec3_unit_vector(&uv, &RAY_DIRECTION(r));
-        t = 0.5f * (uv.y + 1.0f);
-        t_vec3 lerp;
-        vec3_mul_f(&lerp, &g_vec3_identity, (1.0f - t));
-
-        t_vec3 colorbase;
-        vec3_mul_f(&color, &gradient, t);
-
-        vec3_add(&v, &lerp, &colorbase);
-        return (v);
+        vec3_unit_vector(&u, &RAY_DIRECTION(r));
+        t = 0.5f * (u.y  + 1.0f);
+        vec3_add(&tmp, vec3_mul_f(&tmp, &g_vec3_identity, 1.0f - t), vec3_mul_f(&lerp, &gradient, t));
+        return tmp;
     }
 }
 
@@ -89,7 +96,7 @@ int main()
     scene.entities[0] = entity_create(PRIMITIVE_SPHERE, &pos[0], 0.5);
     scene.entities[1] = entity_create(PRIMITIVE_SPHERE, &pos[1], 100);
 
-    int nx = WIN_X, ny = WIN_Y;//, ns = WIN_NS;
+    int nx = WIN_X, ny = WIN_Y, ns = WIN_NS;
     const t_vec3 lower_left_corner = {-2.0f, -1.0f, -1.0f};
     const t_vec3 horizontal = {4.0f, 0.0f, 0.0f};
     const t_vec3 vertical = {0.0f, 2.0f, 0.0f};
@@ -97,14 +104,14 @@ int main()
 
     t_ray r;
     init(&w);
+    ft_bzero(&color, sizeof(t_vec3));
     //PPM HEADER
     //printf("P3\n%d %d\n255\n", nx, ny);
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            ft_bzero(&color, sizeof(t_vec3));
-//            for (int s = 0; s < ns; s++) {
-                float u = (float)i / (float)nx;
-                float v = (float)j  / (float)ny;
+            for (int s = 0; s < ns; s++) {
+                float u = ((float)i + (float)drand48())/ (float)nx;
+                float v = ((float)j + (float)drand48())/ (float)ny;
 
                 t_vec3 v_pos;
                 vec3_mul_f(&v_pos, &vertical, v);
@@ -118,11 +125,10 @@ int main()
                 t_vec3 tmp;
                 ray_assign(&r, &origin, vec3_add(&tmp, &lower_left_corner, &b));
 
-//                t_vec3 color_tmp = rt_color(&r, &scene);
-//                vec3_add(&color, &color, &color_tmp);
-                color = test_render(&r);
-  //          }
-//            vec3_div_f(&color, &color, (float)ns);
+                color = rt_color(&r, &scene);
+                vec3_add(&color, &color, &color);
+            }
+            vec3_div_f(&color, &color, (float)ns);
             t_rgb pixel;
             pixel.r = (int)(255.99 * color.x);
             pixel.g = (int)(255.99 * color.y);
